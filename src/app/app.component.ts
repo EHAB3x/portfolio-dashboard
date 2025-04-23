24154685e8a7e1f4c3bc7a984a6a8718227a958e
EHAB3x <ehabmah6969@gmail.com>
2025-04-23 04:01:39 +0200

    install notifier library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { HeaderComponent } from "./layouts/header/header.component";
+import { NotifierModule } from 'angular-notifier';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NotifierModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
