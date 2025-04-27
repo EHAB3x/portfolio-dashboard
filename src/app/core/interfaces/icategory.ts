f2a7c4da3d0087763eaf6e2cfd00174effcb33d0
EHAB3x <ehabmah6969@gmail.com>
2025-04-28 01:58:31 +0200

    fix shared category component

--- src/app/core/interfaces/icategory.ts
+++
@@ -1,7 +0,0 @@
-export interface ICategory {
-  id: number,
-  description: string,
-  features: string[],
-  icon: string,
-  title: string
-}
--- src/app/pages/education/education.component.ts
+++ src/app/pages/education/education.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-import { CategoryPageComponent } from "../../shared/category-page/category-page.component";
+import { Component, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [CategoryPageComponent],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './education.component.html',
   styleUrl: './education.component.scss'
 })
-export class EducationComponent {
+export class EducationComponent implements OnInit{
+  name !: string;
 
+  searchControl = new FormControl('');
+
+  constructor(private router: Router){
+  }
+
+  ngOnInit(): void {
+    this.name = this.router.url.slice(1);
+
+    this.searchControl.valueChanges
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe({
+      next:(res)=>{
+
+      }
+    })
+  }
 }
