05f28fe0bb3232465c8e4a7d2a63d6b1547f5f36
EHAB3x <ehabmah6969@gmail.com>
2025-05-04 21:37:35 +0200

    finish home skills page

--- src/app/modules/skillsModule/home-skills/home-skills.component.ts
+++ src/app/modules/skillsModule/home-skills/home-skills.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedTableComponent } from "../../../shared/shared-table/shared-table.component";
+import { ISkills } from '../../../core/interfaces/iskills';
+import { Router } from '@angular/router';
+import { SkillsApiService } from '../../../core/services/skills-api.service';
 
 @Component({
   selector: 'app-home-skills',
   standalone: true,
-  imports: [],
+  imports: [SharedTableComponent],
   templateUrl: './home-skills.component.html',
   styleUrl: './home-skills.component.scss'
 })
-export class HomeSkillsComponent {
+export class HomeSkillsComponent implements OnInit{
+  name!: string;
+  skillsData: ISkills[] = [] as ISkills[];
+  searchText: string = '';
 
+  constructor(
+    private router: Router,
+    private SkillService: SkillsApiService
+  ) {}
+
+  ngOnInit(): void {
+    this.name = this.router.url.slice(1);
+
+    this.SkillService.getAllSkills().subscribe({
+      next: (res) => {
+        this.skillsData = res;
+      },
+    });
+  }
 }
