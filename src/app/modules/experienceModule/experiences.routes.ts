f73704ed7b5bc60bad28646c76030023004123f5
EHAB3x <ehabmah6969@gmail.com>
2025-05-12 02:58:08 +0200

    finish home experience pages and components

--- src/app/modules/experienceModule/experiences.routes.ts
+++ src/app/modules/experienceModule/experiences.routes.ts
@@ -1,23 +1,24 @@
 import { Routes } from '@angular/router';
 import { ExperienceComponent } from './experience/experience.component';
+import { HomeExperienceComponent } from './home-experience/home-experience.component';
 
 export const experiencesRoutes: Routes = [
   {
     path: 'experiences',
     component: ExperienceComponent,
-    // children:[
-    //   {
-    //     path:'',
-    //     component: HomeServiceComponent,
-    //   },
-    //   {
-    //     path:'add-experiences',
-    //     component: AddServiceComponent,
-    //   },
-    //   {
-    //     path:'edit-experiences/:experienceId',
-    //     component: EditServiceComponent
-    //   },
-    // ]
+    children:[
+      {
+        path:'',
+        component: HomeExperienceComponent,
+      },
+      // {
+      //   path:'add-experiences',
+      //   component: AddServiceComponent,
+      // },
+      // {
+      //   path:'edit-experiences/:experienceId',
+      //   component: EditServiceComponent
+      // },
+    ]
   }
 ];
