d46a8f7744bad676a25e7cc8228e7593527cce2e
EHAB3x <ehabmah6969@gmail.com>
2025-05-14 21:33:53 +0300

    add spinner to pie chart component

--- src/app/shared/pie-chart/pie-chart.component.ts
+++ src/app/shared/pie-chart/pie-chart.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, OnChanges, ViewEncapsulation } from '@angular/core';
 import { NgxChartsModule, ScaleType } from '@swimlane/ngx-charts';
 import { IHomePage } from '../../core/interfaces/ihome-page';
+import { SpinnerComponent } from '../spinner/spinner.component';
 
 @Component({
   selector: 'app-pie-chart',
   standalone: true,
-  imports: [NgxChartsModule],
+  imports: [NgxChartsModule, SpinnerComponent],
   templateUrl: './pie-chart.component.html',
   styleUrl: './pie-chart.component.scss',
   encapsulation: ViewEncapsulation.None
