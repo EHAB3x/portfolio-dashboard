14833e8af10723282bc3d6a525087cd470c64c63
EHAB3x <ehabmah6969@gmail.com>
2025-05-18 04:35:41 +0300

    create github stats component

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -5,11 +5,12 @@ import { RouterLink } from '@angular/router';
 import { SpinnerComponent } from "../../shared/spinner/spinner.component";
 import { CommonModule } from '@angular/common';
 import { PieChartComponent } from "../../shared/pie-chart/pie-chart.component";
+import { GithubStatsComponent } from "./components/github-stats/github-stats.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterLink, SpinnerComponent, CommonModule, PieChartComponent],
+  imports: [RouterLink, SpinnerComponent, CommonModule, PieChartComponent, GithubStatsComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
