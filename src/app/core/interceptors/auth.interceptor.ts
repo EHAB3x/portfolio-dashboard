8f5deffea0c34cf0c4c2d0a36c9d8d332f7976bd
EHAB3x <ehabmah6969@gmail.com>
2025-05-18 03:00:22 +0300

    avoid adding token to github apis

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -5,6 +5,11 @@ import { AuthServiceService } from '../services/auth-service.service';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthServiceService);
   const token = authService.getToken();
+
+  if (req.url.startsWith('https://api.github.com')) {
+    return next(req);
+  }
+  
   if (token) {
     const clonedReq = req.clone({
       setHeaders: {
