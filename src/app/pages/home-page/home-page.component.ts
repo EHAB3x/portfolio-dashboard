7a00c7bcf335fa9eb0ae01e9cf00722949f9e00d
EHAB3x <ehabmah6969@gmail.com>
2025-05-19 08:08:06 +0300

    create home page cards skeleton

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -5,17 +5,19 @@ import { CommonModule } from '@angular/common';
 import { PieChartComponent } from "../../shared/pie-chart/pie-chart.component";
 import { GithubStatsComponent } from "./components/github-stats/github-stats.component";
 import { HomeCardComponent } from './components/home-card/home-card.component';
+import { CardContentSkeletonComponent } from '../../shared/skeletons/card-content-skeleton/card-content-skeleton.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule, HomeCardComponent ,PieChartComponent, GithubStatsComponent],
+  imports: [CommonModule, HomeCardComponent ,PieChartComponent, GithubStatsComponent, CardContentSkeletonComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent implements OnInit{
   dataLoaded : boolean = false;
   homeCards : IHomePage [] = [] as IHomePage [];
+  cardsLength: number[] = Array(6).fill(1)
 
   constructor(
     private homeService: HomePageService
---
+++ src/app/shared/skeletons/card-content-skeleton/card-content-skeleton.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-content-skeleton',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-content-skeleton.component.html',
+  styleUrl: './card-content-skeleton.component.scss'
+})
+export class CardContentSkeletonComponent {
+
+}
