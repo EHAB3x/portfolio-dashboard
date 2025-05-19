797406373380dbdc5f4c6b04e88dd614a1617ca4
EHAB3x <ehabmah6969@gmail.com>
2025-05-19 08:33:29 +0300

    create github stats cards skeleton

--- src/app/shared/skeletons/card-content-skeleton/card-content-skeleton.component.ts
+++ src/app/shared/skeletons/card-content-skeleton/card-content-skeleton.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-content-skeleton',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card-content-skeleton.component.html',
   styleUrl: './card-content-skeleton.component.scss'
 })
 export class CardContentSkeletonComponent {
-
+  @Input() showButton: boolean = true;
 }
